03bb584322aabd4c08f5d6b3c4f2243c859a606c
lml <soalin228@163.com>
2020-01-23 16:34:57 +0800

    refactor: 代码整理

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 const Koa = require('koa')
 const app = new Koa()
-const views = require('koa-views')
 const json = require('koa-json')
 const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
 const session = require('koa-generic-session')
 const redisStore = require('koa-redis')
+const morgan = require('koa-morgan')
 const path = require('path')
 const fs = require('fs')
-const morgan = require('koa-morgan')
 
 const user = require('./routes/user')
 const blog = require('./routes/blog')
@@ -19,25 +18,12 @@ const { REDIS_CONFIG } = require('./config/db')
 // error handler
 onerror(app)
 
-// middlewares
-app.use(bodyparser({ // 支持多种传数据格式
+// 支持多种传数据格式
+app.use(bodyparser({
   enableTypes:['json', 'form', 'text']
 }))
 app.use(json()) // 返回数据转成json 格式
-app.use(logger()) // 日志
-app.use(require('koa-static')(__dirname + '/public'))
-
-app.use(views(__dirname + '/views', {
-  extension: 'pug'
-}))
-
-// logger 设置控制台打印格式
-app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-})
+app.use(logger()) // 格式化控制台打印log
 
 // logs 日志
 const ENV = process.env.NODE_ENV
